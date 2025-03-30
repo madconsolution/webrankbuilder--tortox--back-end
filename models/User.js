6fd701f0e613838e3ffe5e4afe668a0ac4024a71
Majed Hasan <mdmajedhasan599@gmail.com>
2025-03-30 17:38:07 +0600

    Fixed the email issue

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true },
-    email: { type: String, unique: true },
+    email: { type: String },
     phone: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String },
